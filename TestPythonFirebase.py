c9cd77b89f3cfe27d7db80ac5aa7e753656ac104
V-Grade <vincent.grade@gmail.com>
2017-04-26 23:42:37 +0200

    Add " to data

--- TestPythonFirebase.py
+++ TestPythonFirebase.py
@@ -7,15 +7,15 @@ URL = "https://projet-maison-co-test.firebaseio.com"
 firebase = firebase.FirebaseApplication(URL)
 
 #Creation de la data
-data = {'Temperature': 19, 'Humidite': 42, 'Presence': yes, 'Porte': open,'Heure': time.strftime(%H:%M:%S)}
+data = {'Temperature': 19, 'Humidite': 42, 'Presence': "yes", 'Porte': "open",'Heure': time.strftime("%H:%M:%S")}
 #Envoie de la donnee et recuperation des donnees formater JSON
 result = firebase.patch('salle', data)
 #Affichage de la donnee Value
 
-data = {'Temperature': 15, 'Humidite': 34, 'Presence': no, 'Porte': close, 'Heure': time.strftime(%H:%M:%S)}
+data = {'Temperature': 15, 'Humidite': 34, 'Presence': "no", 'Porte': "close", 'Heure': time.strftime("%H:%M:%S")}
 
 result = firebase.patch('cuisine', data)
 
-data = {'Temperature': 11, 'Humidite': 69, 'Presence': yes, 'Porte': close,'Heure' : time.strftime(%H:%M:%S)}
+data = {'Temperature': 11, 'Humidite': 69, 'Presence': "yes", 'Porte': "close",'Heure' : time.strftime("%H:%M:%S")}
 
 result = firebase.patch('chambre', data)
