128e37db0515776fb363a81f3524027aee5d12cf
V-Grade <vincent.grade@gmail.com>
2017-04-27 18:00:09 +0200

    Better assemblage with day and night

--- Assemblage.py
+++ Assemblage.py
@@ -1,6 +1,7 @@
 import time
-from firebase import firebase
+import grovepi
 from grovepi import *
+from firebase import firebase
 
 #Serveur Firebase
 URL = "https://projet-maison-co-test.firebaseio.com"
@@ -9,48 +10,30 @@ firebase = firebase.FirebaseApplication(URL)
 
 dht_sensor_port = 7
 dht_sensor_type = 0
-ultrasonic_ranger = 3
 light_sensor = 0
 
 while True:
         try:
-                [ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
-                print("temp =", temp, "C\thumidity =", hum,"%")
-		time.sleep(.5)
+                # Get sensor value
+                sensor_value = grovepi.analogRead(light_sensor)
+
+                if sensor_value > 100:
+                        sensor_value = "Jour"
+                else:
+                        sensor_value = "Nuit"
+                print("sensor_value = %s" %(sensor_value))# Get sensor value
+
+        #Get temp and hum
+                [ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)		#Get the temperature and Humidity from the DHT sensor
+                print("temperature =", temp, "humidity =", hum,"%") 
+                time.sleep(.5)
+
 		
         except (IOError,TypeError) as e:
                 print("Error")
 
-
-        try:
-        	# Get sensor value
-        	sensor_value = grovepi.analogRead(light_sensor)
-
-        	print("sensor_value = %d" %(sensor_value))
-        	time.sleep(.5)
-
-    	except IOError:
-        	print ("Error")
-		
-	try:
-        	# Read distance value from Ultrasonic
-        	print(grovepi.ultrasonicRead(ultrasonic_ranger))
-
-	except TypeError:
-		print ("Error")
-	except IOError:
-		print ("Error")
-
 	#Creation de la data
-	data = {'Temperature': temp, 'Humidite': hum, 'Presence': "oui", 'Volet': "ouvert",'Heure': time.strftime("%H:%M:%S")}
+	data = {'Temperature': temp, 'Humidite': hum, 'Presence': "oui", 'Volet': "ouvert", 'Exterieur' : sensor_value, 'Heure': time.strftime("%H:%M")}
 	#Envoie de la donnee et recuperation des donnees formater JSON
 	result = firebase.patch('/data/salle', data)
 	#Affichage de la donnee Value
-
-		data = {'Temperature': 15, 'Humidite': 34, 'Presence': "non", 'Volet': "ferme", 'Heure': time.strftime("%H:%M:%S")}
-
-		result = firebase.patch('/data/cuisine', data)
-
-		data = {'Temperature': 11, 'Humidite': 69, 'Presence': "yes", 'Volet': "ouvert",'Heure' : time.strftime("%H:%M:%S")}
-
-		result = firebase.patch('/data/chambre', data)
