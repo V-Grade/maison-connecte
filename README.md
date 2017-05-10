3bf9451f57f8ff3adeae3fe288b58b792cf9c7ea
Mogglio <mathieudeghilage@gmail.com>
2017-05-10 15:35:28 +0200

    Amélioration

--- README.md
+++
@@ -1,2 +0,0 @@
-# maison-connecte
-Projet de fin d'année en L3 Web par V.GRADE et M.DEGHILAGE - création d'une maison connecté
--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-	// create the module and name it scotchApp, ngRoute et firebase sont des dépendances
-	var scotchApp = angular.module('scotchApp', ['ngRoute','firebase']);
+// create the module and name it scotchApp, ngRoute et firebase sont des dépendances
+var scotchApp = angular.module('scotchApp', ['ngRoute','firebase']);
 
-	// configure our routes
-	// .config ajoute une action lors du chargement du module
-	scotchApp.config(function($routeProvider) {
-		$routeProvider
+// configure our routes
+// .config ajoute une action lors du chargement du module
+scotchApp.config(function($routeProvider) {
+	$routeProvider
 
-			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/piece1.html',
-				controller  : 'mainController'
-			})
+		// route for the home page
+		.when('/', {
+			templateUrl : 'pages/piece1.html',
+			controller  : 'mainController'
+		})
 
-			// route for the about page
-			.when('/piece2', {
-				templateUrl : 'pages/piece2.html',
-				controller  : 'mainController'
-			})
+		// route for the about page
+		.when('/piece2', {
+			templateUrl : 'pages/piece2.html',
+			controller  : 'mainController'
+		})
 
-			// route for the contact page
-			.when('/piece3', {
-				templateUrl : 'pages/piece3.html',
-				controller  : 'mainController'
-			});
-	});
+		// route for the contact page
+		.when('/piece3', {
+			templateUrl : 'pages/piece3.html',
+			controller  : 'mainController'
+		});
+});
 
-	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope, $firebase) {
-		// create a message to display in our view
-		//$scope.message = 'Everyone come and see how good I look!';
-		// connect to firebase 
-		var ref = new Firebase("https://projet-maison-co-test.firebaseio.com/data");  
-		var fb = $firebase(ref);
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope, $firebase) {
+	// create a message to display in our view
+	//$scope.message = 'Everyone come and see how good I look!';
+	// connect to firebase 
+	var ref = new Firebase("https://projet-maison-co-test.firebaseio.com/data");  
+	var fb = $firebase(ref);
 
-		// sync as object 
-		var syncObject = fb.$asObject();
+	// sync as object 
+	var syncObject = fb.$asObject();
 
-		// three way data binding
-		syncObject.$bindTo($scope, 'data');
-	});
+	// three way data binding
+	syncObject.$bindTo($scope, 'data');
+});
 
 
