9b9bee97736c8c9a71b0f1381b4ecaa78f926270
V-Grade <vincent.grade@gmail.com>
2017-05-14 15:07:32 +0200

    add more sensor

--- Assemblage.py
+++ Assemblage.py
@@ -8,12 +8,24 @@ URL = "https://projet-maison-co-test.firebaseio.com"
 #Connexion au serveur
 firebase = firebase.FirebaseApplication(URL)
 
-dht_sensor_port = 7
-dht_sensor_type = 0
-light_sensor = 0
+dht_sensor_port_salle = 7 # Connect the DHt sensor to port D7
+dht_sensor_type_salle = 0
+
+dht_sensor_port_cuisine = 5 # Connect the DHt sensor to port D5
+dht_sensor_type_cuisine = 0
+
+dht_sensor_port_chambre = 6 # Connect the DHt sensor to port D6
+dht_sensor_type_chambre = 0
+
+light_sensor = 0 # Connect the light sensor to port A0
 ultrasonic_ranger = 3 # Connect to D3
 presence = "non"
-led = 4
+led_salle = 4 # Connect the LED to port D4
+led_salle_value = 0
+led_cuisine = 2 # Connect the LED to port D2
+led_cuisine_value = 0
+led_chambre = 8 # Connect the LED to port D8
+led_chambre_value = 0
 
 while True:
         try:
@@ -26,10 +38,20 @@ while True:
                         sensor_value = "Nuit"
                 print("sensor_value = %s" %(sensor_value))# Get sensor value
 
-                #Get temp and hum
-                [ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
-                print("temperature =", temp, "humidity =", hum,"%") 
-                time.sleep(.5)
+                #Get temp and hum salle
+                [ temp_salle,hum_salle ] = dht(dht_sensor_port_salle,dht_sensor_type_salle)
+                print("temperature =", temp_salle, "humidity =", hum_salle,"%") 
+                time.sleep(.2)
+
+                #Get temp and hum cuisine
+                [ temp_cuisine,hum_cuisine ] = dht(dht_sensor_port_cuisine,dht_sensor_type_cuisine)
+                print("temperature =", temp_cuisine, "humidity =", hum_cuisine,"%") 
+                time.sleep(.2)
+
+                #Get temp and hum chambre
+                [ temp_chambre,hum_chambre ] = dht(dht_sensor_port_chambre,dht_sensor_type_chambre)
+                print("temperature =", temp_chambre, "humidity =", hum_chambre,"%") 
+                time.sleep(.2)
                 
                 #Get Ultrasonic
 		ultrasonic_value = grovepi.ultrasonicRead(ultrasonic_ranger)
@@ -41,15 +63,48 @@ while True:
 		print("ultrasonic_value =", ultrasonic_value)
                 #Light on auto
 		if sensor_value == "Nuit" and presence == "oui":
-			digitalWrite(led,1)
+			digitalWrite(led_salle,1)
+			digitalWrite(led_cuisine,1)
+			digitalWrite(led_chambre,1)
+
+			led_salle_vallue = "Allumee"
+			print("lumiere =", led_salle_vallue)
+			time.sleep(.1)
+			led_cuisine_vallue = "Allumee"
+			print("lumiere =", led_cuisine_vallue)
+			time.sleep(.1)
+			led_chambre_vallue = "Allumee"
+			print("lumiere =", led_chambre_vallue)
+			time.sleep(.1)
 		else:
-			digitalWrite(led,0)
+                        digitalWrite(led_salle,0)
+                        digitalWrite(led_cuisine,0)
+			digitalWrite(led_chambre,0)
+
+			led_salle_vallue = "Eteinte"
+			print("lumiere =", led_salle_vallue)
+			time.sleep(.1)
+			led_cuisine_vallue = "Eteinte"
+			print("lumiere =", led_cuisine_vallue)
+			time.sleep(.1)
+			led_chambre_vallue = "Eteinte"
+			print("lumiere =", led_chambre_vallue)
+			time.sleep(.1)
 		
         except (IOError,TypeError) as e:
                 print("Error")
 
-	#Creation de la data
-	data = {'Temperature': temp, 'Humidite': hum, 'Presence': presence, 'Volet': "ouvert", 'Exterieur' : sensor_value, 'Heure': time.strftime("%H:%M")}
-	#Envoie de la donnee et recuperation des donnees formater JSON
+	#Creation de la data salle
+	data = {'Temperature': temp_salle, 'Humidite': hum_salle, 'Presence': presence, 'Lumiere' : led_salle_vallue , 'Exterieur' : sensor_value, 'Heure': time.strftime("%H:%M")}
+	#Envoie de la donnee
 	result = firebase.patch('/data/salle', data)
-	#Affichage de la donnee Value
+
+	#Creation de la data cuisine
+	data = {'Temperature': temp_cuisine, 'Humidite': hum_cuisine, 'Presence': presence, 'Lumiere' : led_cuisine_vallue, 'Exterieur' : sensor_value, 'Heure': time.strftime("%H:%M")}
+	#Envoie de la donnee
+	result = firebase.patch('/data/cuisine', data)
+
+	#Creation de la data chambre
+	data = {'Temperature': temp_chambre, 'Humidite': hum_chambre, 'Presence': presence, 'Lumiere' : led_chambre_vallue, 'Exterieur' : sensor_value, 'Heure': time.strftime("%H:%M")}
+	#Envoie de la donnee
+	result = firebase.patch('/data/chambre', data)
